1e232a0adfdf431943ce9700714fe1083e886b88
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-04 07:27:23 -0500

    Create view-coaches.php

---
+++ view-coaches.php
@@ -0,0 +1,32 @@
+<h1>Coaches</h1>
+<div class="table-responsive">
+  <table class="table">
+    
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Position</th>
+        <th>Age</th>
+      </tr>
+    </thead>
+      
+      <tbody>
+        <?php
+          while ($coach = $coaches->_assoc()) {
+        ?>
+            <tr>
+
+                <td><?php echo $coach['coaches_id']; ?></td>
+                <td><?php echo $coach['coaches_name']; ?></td> 
+                <td><?php echo $coach['coaches_position']; ?></td>
+                <td><?php echo $coach['coaches_age']; ?></td>
+              
+            </tr>
+
+        <?php
+          }
+        ?>          
+      </tbody>
+  </table>
+</div>
