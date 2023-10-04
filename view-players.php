8f7fc24662abdc2eadab0e19760d0fa474f6bd3d
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-04 09:12:49 -0500

    Update view-players.php

--- view-players.php
+++ view-players.php
@@ -1,1 +1,32 @@
+<h1>Players</h1>
+<div class="table-responsive">
+  <table class="table">
+    
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Position</th>
+        <th>Age</th>
+      </tr>
+    </thead>
+      
+      <tbody>
+        <?php
+          while ($player = $players->fetch_assoc()) {
+        ?>
+            <tr>
 
+                <td><?php echo $coach['coaches_id']; ?></td>
+                <td><?php echo $coach['coach_name']; ?></td> 
+                <td><?php echo $coach['coach_position']; ?></td>
+                <td><?php echo $coach['coach_age']; ?></td>
+              
+            </tr>
+
+        <?php
+          }
+        ?>          
+      </tbody>
+  </table>
+</div>
