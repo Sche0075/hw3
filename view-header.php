0327eec6ba6002ad47541fe815ff657b847f1555
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-04 06:08:12 -0500

    Update view-header.php
    updated nav bar

--- view-header.php
+++ view-header.php
@@ -20,16 +20,13 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="post.php">Post</a>
+          <a class="nav-link" href="coaches.php">Coaches</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="post-result.php">Post Result</a>
+          <a class="nav-link" href="players.php">Players</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="get.php">Get</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="get-result.php">Get result</a>
+          <a class="nav-link" href="coaches-with-players.php">Coaches with Players</a>
         </li>
       </ul>
     </div>
