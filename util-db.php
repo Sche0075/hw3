0eb41904160db00db80681b2a13fb5d26246f4f6
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-04 07:51:21 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('138.68.5.11', 'scheetzo_hw3user', 'Lance4906!!!', 'scheetzo_hw3');
+    $conn = new mysqli('138.68.5.11', 'scheetzo_hw3user', "Lance4906!", 'scheetzo_hw3');
     
     // Check connection
     if ($conn->connect_error) {
