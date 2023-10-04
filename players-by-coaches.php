e222899c1920d4e379ad50aab11e55f0641a72b1
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-04 09:23:59 -0500

    Update players-by-coaches.php

--- players-by-coaches.php
+++ players-by-coaches.php
@@ -1,1 +1,11 @@
+<?php
+require_once("util-db.php");
+require_once("model-players-by-coaches.php");
 
+$pageTitle = "Coaches";
+include "view-header.php";
+
+$coaches = selectCoaches();
+include "view-coaches.php";
+include "view-footer.php";
+?>
