b38fa437d6b8c7a654cd0cfab3c57357a143b124
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-04 11:14:07 -0500

    Update coaches-with-players.php

--- coaches-with-players.php
+++ coaches-with-players.php
@@ -1,1 +1,12 @@
 
+<?php
+require_once("util-db.php");
+require_once("model-coaches-with-players.php");
+
+$pageTitle = "Coaches with their Players";
+include "view-header.php";
+
+$coaches = selectCoaches();
+include "view-coaches-with-players.php";
+include "view-footer.php";
+?>
