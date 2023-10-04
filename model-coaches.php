9e9cae692ffab79e120e123638c1aac8f1c3be57
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-04 08:18:02 -0500

    Update model-coaches.php

--- model-coaches.php
+++ model-coaches.php
@@ -4,7 +4,6 @@ function selectCoaches() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT coaches_id, coach_name, coach_position, coach_age FROM `coaches`");
-        $stmt->bind_param("s", $uEmail);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
