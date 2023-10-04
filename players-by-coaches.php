52b079839dda99a1a1f653d53a8b7c2af8f0fc23
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-04 10:29:07 -0500

    Update players-by-coaches.php

--- players-by-coaches.php
+++ players-by-coaches.php
@@ -5,7 +5,7 @@ require_once("model-players-by-coaches.php");
 $pageTitle = "Players by Coaches";
 include "view-header.php";
 
-$coaches = selectPlayersByCoaches($_GET['id']);
+$players = selectPlayersByCoaches($_GET['id']);
 include "view-players-by-coaches.php";
 include "view-footer.php";
 ?>
