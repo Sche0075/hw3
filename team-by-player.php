0d68457f4a4f18bdddbd45b990ee72a08a53f37d
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-04 10:35:07 -0500

    Update team-by-player.php

--- team-by-player.php
+++ team-by-player.php
@@ -5,7 +5,7 @@ require_once("model-team-by-player.php");
 $pageTitle = "Teams by Players";
 include "view-header.php";
 
-$team = selectTeamByPlayers($_GET['cid']);
+$team = selectTeamByPlayer($_GET['cid']);
 include "view-team-by-player.php";
 include "view-footer.php";
 ?>
