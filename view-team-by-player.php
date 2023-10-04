6827565dcceeda0fd5b43a4013111a20db116bc6
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-04 10:40:34 -0500

    Update view-team-by-player.php

--- view-team-by-player.php
+++ view-team-by-player.php
@@ -16,16 +16,16 @@
       
       <tbody>
         <?php
-          while ($teams = $teams->fetch_assoc()) {
+          while ($team = $teams->fetch_assoc()) {
         ?>
             <tr>
 
-                <td><?php echo $player['player_id']; ?></td>
-                <td><?php echo $player['player_name']; ?></td> 
-                <td><?php echo $player['player_position']; ?></td>
-                <td><?php echo $player['player_age']; ?></td>
-                <td><?php echo $player['level']; ?></td>
-                <td><?php echo $player['practice_time']; ?></td>
+                <td><?php echo $team['player_id']; ?></td>
+                <td><?php echo $team['player_name']; ?></td> 
+                <td><?php echo $team['player_position']; ?></td>
+                <td><?php echo $team['player_age']; ?></td>
+                <td><?php echo $team['level']; ?></td>
+                <td><?php echo $team['practice_time']; ?></td>
               
             </tr>
 
