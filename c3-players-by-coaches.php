578718aaa12ca2b53486912eaf53f0e31429b0a3
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-23 13:39:48 -0500

    Update c3-players-by-coaches.php

--- c3-players-by-coaches.php
+++ c3-players-by-coaches.php
@@ -3,9 +3,9 @@ require_once("b1-util-db.php");
 require_once("m3-model-players-by-coaches.php");
 
 $pageTitle = "Players by Coaches";
-include "a1-view-header.php";
+include "a2-view-header.php";
 
 $players = selectPlayersByCoaches($_GET['id']);
-include "a3-view-players-by-coaches.php";
+include "v3-view-players-by-coaches.php";
 include "a3-view-footer.php";
 ?>
