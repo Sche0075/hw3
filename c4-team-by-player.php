900a1d7fe9c7f88b3365dbf2f018285cc80b1862
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-23 13:43:47 -0500

    Update c4-team-by-player.php

--- c4-team-by-player.php
+++ c4-team-by-player.php
@@ -1,11 +1,11 @@
 <?php
-require_once("util-db.php");
-require_once("model-team-by-player.php");
+require_once("b1-util-db.php");
+require_once("m4-model-team-by-player.php");
 
 $pageTitle = "Teams by Players";
-include "view-header.php";
+include "a2-view-header.php";
 
 $teams = selectTeamByPlayer($_POST['cid']);
-include "view-team-by-player.php";
-include "view-footer.php";
+include "v4-view-team-by-player.php";
+include "a3-view-footer.php";
 ?>
