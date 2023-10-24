2104b6c265e378736edea5939fc33335442f64ea
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-24 17:18:31 -0500

    Update v6-view-leagues.php

--- v6-view-leagues.php
+++ v6-view-leagues.php
@@ -1,1 +1,35 @@
+<h1>Leagues - model not updated</h1>
+<div class="table-responsive">
+  <table class="table">
+    
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Position</th>
+        <th>Age</th>
+        <th></th>
+        
+      </tr>
+    </thead>
+      
+      <tbody>
+        <?php
+          while ($coach = $coaches->fetch_assoc()) {
+        ?>
+            <tr>
 
+                <td><?php echo $coach['coaches_id']; ?></td>
+                <td><?php echo $coach['coach_name']; ?></td> 
+                <td><?php echo $coach['coach_position']; ?></td>
+                <td><?php echo $coach['coach_age']; ?></td>
+                <td><a href="c3-players-by-coaches.php?id=<?php echo $coach['coaches_id']; ?>">Players</a></td>
+              
+            </tr>
+
+        <?php
+          }
+        ?>          
+      </tbody>
+  </table>
+</div>
