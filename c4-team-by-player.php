ea04f2c72fa6178be86effc183fd2ed640cba7f7
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-24 17:58:05 -0500

    Update c4-team-by-player.php

--- c4-team-by-player.php
+++ c4-team-by-player.php
@@ -5,7 +5,7 @@ require_once("m4-model-team-by-player.php");
 $pageTitle = "Teams by Players";
 include "a2-view-header.php";
 
-$teams = selectTeamByPlayer($_POST['cid']);
+$teams = selectTeamByPlayer($_POST['tid']);
 include "v4-view-team-by-player.php";
 include "a3-view-footer.php";
 ?>
