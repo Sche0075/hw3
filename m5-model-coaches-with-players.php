f4dba49660dcab45fbb93eaf07530413821fa4e0
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-24 17:25:39 -0500

    Update m5-model-coaches-with-players.php

--- m5-model-coaches-with-players.php
+++ m5-model-coaches-with-players.php
@@ -14,11 +14,11 @@ function selectCoaches() {
     }
 }
 
-function selectPlayersByCoaches($iid) {
+function selectPlayersByCoaches($pid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT p.player_id, player_name, player_position, player_age, level, practice_time FROM `player` p join team t on t.player_id = p.player_id where t.coach_id = ?");
-        $stmt->bind_param("i",$iid);
+        $stmt->bind_param("i",$pid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
