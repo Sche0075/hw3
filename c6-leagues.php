f804d2afd84acfa6b826116f938d877c231e388b
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-24 21:46:13 -0500

    Update c6-leagues.php

--- c6-leagues.php
+++ c6-leagues.php
@@ -1,1 +1,11 @@
+<?php
+require_once("b1-util-db.php");
+require_once("m2-model-players.php");
 
+$pageTitle = "Players";
+include "a2-view-header.php";
+
+$players = selectPlayers();
+include "v2-view-players.php";
+include "a3-view-footer.php";
+?>
