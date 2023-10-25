8a16f59103e35359408c659953ccb64f23e21b26
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-25 12:34:25 -0500

    Update c1-coaches.php

--- c1-coaches.php
+++ c1-coaches.php
@@ -14,6 +14,14 @@ if (isset($_POST['actionType'])) {
        echo '<div class="alert alert-danger" role="alert">Error.</div>';
      }
         break;    
+   
+    case "Add":
+     if (deleteCoaches($_POST['cid'])) {
+       echo '<div class="alert alert-success" role="alert">Course deleted.</div>';
+     } else {
+       echo '<div class="alert alert-danger" role="alert">Error.</div>';
+     }
+        break;    
   }  
 }
 
