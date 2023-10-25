7bcbae452580c49d26e6eee1873070482373df8d
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-25 07:03:06 -0500

    Update n1-newform-coaches.php

--- n1-newform-coaches.php
+++ n1-newform-coaches.php
@@ -24,12 +24,12 @@
             <div class="mb-3">
               <label for="cPosition" class="form-label">Coach Position</label>
               <input type="text" class="form-control" id="cPosition" name="cPosition">
-            </div>
+            <>
           
             <div class="mb-3">
               <label for="cAge" class="form-label">Coach Age</label>
               <input type="text" class="form-control" id="cAge" name="cAge">
-            </div>   
+            <>   
           
               <input type="hidden" name="actionType" value="Add">
             <button type="submit" class="btn btn-primary">Save</button>
