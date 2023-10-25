4bee06bd1a9d81ac88680b7d757693fc2985d636
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-25 12:51:13 -0500

    Update v1-view-coaches.php

--- v1-view-coaches.php
+++ v1-view-coaches.php
@@ -38,7 +38,7 @@
                 <td></td>
                 <td>
                     <form method="post" action="">
-                        <input type="hidden" name="cid" value="<?php echo $coach['coach_id']; ?>">
+                        <input type="hidden" name="cid" value="<?php echo $coach['coaches_id']; ?>">
                         <input type="hidden" name="actionType" value="Delete">
                         <button type="submit" class="btn btn-primary">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
