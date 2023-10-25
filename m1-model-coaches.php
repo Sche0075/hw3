890688bca3a355f11a2a03f5cc8a0774f6814773
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-25 01:27:04 -0500

    Update m1-model-coaches.php

--- m1-model-coaches.php
+++ m1-model-coaches.php
@@ -14,4 +14,46 @@ function selectCoaches() {
     }
 }
 
+function insertCoaches($cName, $cAgegroup, $cAge) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `coaches` (`coach_name`, `coach_position`, `coach_age`) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $cName, $cAgegroup, $cAge);
+        $succcess = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateCoaches($cName, $cAgegroup, $cAge) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `coaches` (`coach_name`, `coach_position`, `coach_age`) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $cName, $cAgegroup, $cAge);
+        $succcess = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteCoaches($cName, $cAgegroup, $cAge, $cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `coaches` (`coach_name`, `coach_position`, `coach_age`) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $cName, $cAgegroup, $cAge);
+        $succcess = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
