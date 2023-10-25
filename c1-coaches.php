d2780e0a4a350c8258d74afa56112b7a3daa8fe0
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-25 05:06:39 -0500

    Update c1-coaches.php

--- c1-coaches.php
+++ c1-coaches.php
@@ -5,13 +5,7 @@ require_once("m1-model-coaches.php");
 $pageTitle = "Coaches";
 include "a2-view-header.php";
 
-if (isset($_POST['actionType'])) {
-  switch ($_POST['actionType']) {
-    case "Add":
-      insertCoaches($_POST['cName'], $_POST['cPosition'], $POST['cAge']);
-        break;    
-  }
-}
+
 
 $coaches = selectCoaches();
 include "v1-view-coaches.php";
