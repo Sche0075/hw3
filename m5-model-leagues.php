92e874832d00cc70647fa661c3a5a2134f7a32ec
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-24 21:47:43 -0500

    Update m5-model-leagues.php

--- m5-model-leagues.php
+++ m5-model-leagues.php
@@ -1,1 +1,17 @@
+<?php
 
+function selectPlayers() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT player_id, player_name, player_position, player_age FROM `player`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
