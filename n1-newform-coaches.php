396493ff4d9fba17e38827c57f4df2b13934e858
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-25 07:33:49 -0500

    Update n1-newform-coaches.php

--- n1-newform-coaches.php
+++ n1-newform-coaches.php
@@ -31,7 +31,7 @@
                                       <input type="text" class="form-control" id="cAge" name="cAge">
                                     </div>   
                                   
-                                <input type="hidden" name="actionType" value="Add">
+                                <input type="hidden" name="actionType" value="Add"</input>
                               <button type="submit" class="btn btn-primary">Save</button>
                           </form> 
                   </div>      
