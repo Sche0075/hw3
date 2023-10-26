82b1d69169a0bbccc8f28151bc48c742943d919a
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-25 21:36:07 -0500

    Update m2-model-players.php

--- m2-model-players.php
+++ m2-model-players.php
@@ -14,4 +14,46 @@ function selectPlayers() {
     }
 }
 
+function insertPlayers($pName, $pPosition, $pAge) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `player` (`player_name`, `player_position`, `player_age`) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $pName, $pPosition, $pAge);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updatePlayers($pName, $pPosition, $pAge, $pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `player` set `player_name` = ?, `player_position` = ?, `player_age` = ? where player_id = ?");
+        $stmt->bind_param("sssi", $pName, $pPosition, $pAge, $pid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deletePlayers($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from coaches where player_id = ?");
+        $stmt->bind_param("i", $pid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
