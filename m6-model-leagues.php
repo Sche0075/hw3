4112b531058ed75dc37e5dfe59ac9940939f8d10
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-25 22:21:11 -0500

    Update m6-model-leagues.php

--- m6-model-leagues.php
+++ m6-model-leagues.php
@@ -14,4 +14,46 @@ function selectLeagues() {
     }
 }
 
+function insertLeagues($lDirector, $lAgegroup, $lLevel) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `league` (`league_director`, `league_agegroup`, `league_level`) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $lDirector, $lAgegroup, $lLevel);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateLeagues($lDirector, $lAgegroup, $lLevel, $lid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `league` set `league_director` = ?, `league_agegroup` = ?, `league_level` = ? where league_id = ?");
+        $stmt->bind_param("sssi", $lDirector, $lAgegroup, $lLevel, $lid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteLeagues($lid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from league where league_id = ?");
+        $stmt->bind_param("i", $lid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
