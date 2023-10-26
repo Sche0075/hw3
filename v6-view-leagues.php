d803a53950dcd30647a20256242d2b0b364391be
Sche0075 <142837904+Sche0075@users.noreply.github.com>
2023-10-25 22:26:26 -0500

    Update v6-view-leagues.php

--- v6-view-leagues.php
+++ v6-view-leagues.php
@@ -1,4 +1,13 @@
-<h1>Leagues</h1>
+<div class="row">
+  <div class="col">
+      <h1>Leagues</h1>
+  </div>
+  <div class="col-auto">
+<?php
+    include "n3-newform-leagues.php";
+?>
+  </div>
+</div>
 <div class="table-responsive">
   <table class="table">
     
